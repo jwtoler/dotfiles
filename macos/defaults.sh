002894db9ca0ae6b683d32e050cc171c584ace97
Justin Toler <17133784+jwtoler@users.noreply.github.com>
2023-06-22 07:52:28 -0400

    Updates to zsh and git config\

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# Close any open System Preferences panes to prevent them from overriding changes
+# Thanks to Mathias Bynens!
+# ~/.macos — https://mths.be/macos
+
+# Close any open System Preferences panes to prevent them from overriding
+# settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
 
 # Ask for the administrator password upfront
