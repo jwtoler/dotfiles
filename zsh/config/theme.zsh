d8c386295d0d963ee9873bf2bbc1c16212ed88b7
Justin Toler <17133784+jwtoler@users.noreply.github.com>
2023-06-30 13:29:20 +0000

    move zsh theme

--- zsh/config/theme.zsh
+++ zsh/config/theme.zsh
@@ -6,5 +6,5 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 
 source $ZSH/oh-my-zsh.sh
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+# To customize prompt, run `p10k configure` or edit p10k.zsh.
 source $DOTFILES/zsh/themes/p10k.zsh
